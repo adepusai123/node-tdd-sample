292f28f8f428b00c9347716e6344ee48b5abaace
Sai Kumar <saikumara@wavelabs.in>
2018-11-14 12:59:32 +0530

    +ve case with mocks

--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -16,7 +16,7 @@ describe("AppController", function () {
             const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(true);
             // pass user into the req object
             let req = {
-                user:user
+                user: user
             };
             // Have `res` have a send key with a function value coz we use `res.send()` in our func
             let res = {
@@ -33,22 +33,22 @@ describe("AppController", function () {
             expect(isLoggedInStub.calledOnce).to.be.true;
         });
 
-        it("should send something else when user is not logged in",function(){
+        it("should send something else when user is not logged in", function () {
             let user = {
-                isLoggedIn:function(){}
+                isLoggedIn: function () { }
             }
             //stub isLoggedIn function and make it return false always
-            const isLoggedInStub = sinon.stub(user,"isLoggedIn").returns(false);
+            const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(false);
             //pass user into req object
             let req = {
-                user:user
+                user: user
             }
             //Have res have a send key with funciton value coz we res.send() in our func
             let res = {
                 // replace empty function with a spy
-                send:sinon.spy()
+                send: sinon.spy()
             }
-            indexPage.getIndexPage(req,res);
+            indexPage.getIndexPage(req, res);
             // let's see what we get on res.send
             // console.log(res.send);
             // `res.send` called once
@@ -57,16 +57,43 @@ describe("AppController", function () {
             // assert that the stub is logged in at least once
             expect(isLoggedInStub.calledOnce).to.be.true;
         });
+
+        it("should send hey when user is logged in with mocks", function () {
+            // instantiate a user object with an empty isLoggedIn function
+            let user = {
+                isLoggedIn: function () { }
+            }
+            // Stub isLoggedIn function and make it return true always
+            const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(true);
+            // pass user into the req object
+            let req = {
+                user: user
+            };
+            // Have `res` have a send key with a function value coz we use `res.send()` in our func
+            let res = {
+                send: function () { }
+            }
+
+            //mock res
+            const mock = sinon.mock(res);
+            //build how we expect it t work
+            mock.expects("send").once().withExactArgs("Hey");
+        
+            indexPage.getIndexPage(req, res);
+            expect(isLoggedInStub.calledOnce).to.be.true;
+            // verify that mock works as expected
+            mock.verify();
+        });
     });
 
-/*     describe("User", function () {
-        describe("addUser", function () {
-            it("should add a user", function () {
-                sinon.spy(user, "addUser");
-                user.addUser('Sai Kumar');
-                // lets log `addUser` and see what we get
-                console.log(user.addUser);
+    /*     describe("User", function () {
+            describe("addUser", function () {
+                it("should add a user", function () {
+                    sinon.spy(user, "addUser");
+                    user.addUser('Sai Kumar');
+                    // lets log `addUser` and see what we get
+                    console.log(user.addUser);
+                });
             });
-        });
-    }); */
+        }); */
 });
