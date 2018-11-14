c82e9a051e734cce1458767eefce3ff287e2a65d
Sai Kumar <saikumara@wavelabs.in>
2018-11-14 15:37:49 +0530

    Async with Promise

--- tests/asyncFunc.test.js
+++ tests/asyncFunc.test.js
@@ -24,3 +24,21 @@ describe.only("AsyncTest",function(){
         });
     });
 });
+
+chai.use(require("chai-as-promised"));
+// This is just an async func that takes in a bool
+// and that returns a promise
+function someMadeUpAyncFuncPromise(boolValue, cb) {
+    return new Promise(function(resolve){
+      setTimeout(function() {
+        resolve(boolValue ? "You get a sweet :)" : "You get nothing!!");
+      }, 0);
+    })
+  }
+
+  // Added the `only` tag to have only this set of tests to run
+  describe.only("AsyncTestPromise", function()  {
+      it("should return `You get a sweet :)` if `true` is passed in with Promise",function(){
+          expect(someMadeUpAyncFuncPromise(true)).to.eventually.equal("You get a sweet :)");
+      });
+  });
