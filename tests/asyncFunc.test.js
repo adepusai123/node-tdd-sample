d0bcae38483e234608de837f1aabae7cbbcf11e8
Sai Kumar <saikumara@wavelabs.in>
2018-11-14 15:21:31 +0530

    Testing Async function with module functions

---
+++ tests/asyncFunc.test.js
@@ -0,0 +1,22 @@
+const chai = require("chai");
+const expect = chai.expect;
+// This is just an async func that takes in a bool
+// and calls a callback that returns a some message
+// depending on the bool value
+function someMadeUpAsyncFunc(boolValue, cb){
+    setTimeout(function(){
+        cb(boolValue ? "You get a sweet :)" : "You get nothing!!");
+    },0);
+}
+
+// Added the `only` tag to have only this set of tests to run
+describe.only("AsyncTest",function(){
+    it("should return `You get a sweet :)` if `true` is passed in",function(){
+        someMadeUpAsyncFunc(true,function(sweetcheck){
+            expect(sweetcheck).to.equal("You get a sweet :)");
+        });
+    });
+    it("should return `You get nothing!!` if `false` is passed in",function(){
+        expect(someMadeUpAsyncFunc(false)).to.eventually.equal("You get nothing!!");
+    });
+});
