673a6812c6f2fa2f66a7300045070e1096b58d7c
Sai Kumar <saikumara@wavelabs.in>
2018-11-14 11:55:27 +0530

    spie is a function will give count of executions and output

--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -4,20 +4,37 @@ const expect = chai.expect;
 const sinon = require("sinon");
 //import our getIndexPage function
 const indexPage = require('../../controllers/app.controller.js');
+const user = {
+    addUser: (name) => {
+        this.name = name;
+    }
+};
+describe("AppController",function(){
+    describe("getIndexPage", function () {
+        it("should send hey", function () {
+            let req = {};
+            // Have `res` have a send key with a function value coz we use `res.send()` in our func
+            let res = {
+                send: sinon.spy()
+            }
+            indexPage.getIndexPage(req, res);
+            // let's see what we get on res.send
+            console.log(res.send);
+            // `res.send` called once
+            expect(res.send.calledOnce).to.be.true;
+            // expect to get argument `bla` on first call
+            expect(res.send.firstCall.args[0]).to.equal("Hey");
+        });
+    });
 
-describe("getIndexPage",function(){
-    it("should return index page",function(){
-        let req={};
-        // Have `res` have a send key with a function value coz we use `res.send()` in our func
-        let res = {
-            send: sinon.spy()
-        }
-        indexPage.getIndexPage(req,res);
-        // let's see what we get on res.send
-        console.log(res.send);
-        // `res.send` called once
-        expect(res.send.calledOnce).to.be.true;
-        // expect to get argument `bla` on first call
-        expect(res.send.firstCall.args[0]).to.equal("hey");
+    describe("User",function(){
+        describe("addUser",function(){
+            it("should add a user", function(){
+                sinon.spy(user,"addUser");
+                user.addUser('Sai Kumar');
+                // lets log `addUser` and see what we get
+                console.log(user.addUser);
+            });
+        });
     });
 });
